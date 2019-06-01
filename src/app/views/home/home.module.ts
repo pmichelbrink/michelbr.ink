18d03372e962a119b2c77fcaed3b352ba404014f
pmichelbrink <pmichelbrink@gmail.com>
2019-05-31 23:44:44 -0600

    Cleaned up old references

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -22,27 +22,15 @@ import { HomeRoutes } from "./home.routing";
 
 
 import { HomeComponent } from './home.component';
-import { HomeOneComponent } from './home-one.component';
-import { HomeTwoComponent } from './home-two.component';
 import { HomeBlogComponent } from './home-blog.component';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { IntroOneComponent } from './intro-one/intro-one.component';
-import { IntroTwoComponent } from './intro-two/intro-two.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogsComponent } from './blogs/blogs.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ServicesComponent } from './services/services.component';
-import { CtaComponent } from './cta/cta.component';
-import { PricingsComponent } from './pricings/pricings.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { PortfolioCarouselComponent } from './projects/projects.component';
-import { TestimonialsCarouselComponent } from './testimonials-carousel/testimonials-carousel.component';
-import { ServicesCarouselComponent } from './services-carousel/services-carousel.component';
 import { DemoComponent } from './demo/demo.component';
-import { AllSectionsComponent } from './all-sections.component';
 
 import { WINDOW_PROVIDERS } from '../../shared/helpers/window.helper';
 
@@ -69,27 +57,15 @@ import { WINDOW_PROVIDERS } from '../../shared/helpers/window.helper';
   ],
   declarations: [
     HomeComponent,
-    HomeOneComponent,
-    HomeTwoComponent, 
     HomeBlogComponent,
     HeaderComponent, 
     HomePageComponent,
-    IntroOneComponent,
-    IntroTwoComponent,
     BlogListComponent,
     BlogsComponent,  
-    PortfolioComponent, 
-    ServicesComponent, 
-    CtaComponent, 
-    PricingsComponent, 
     ContactComponent, 
     FooterComponent, 
-    TestimonialsComponent, 
-    PortfolioCarouselComponent,  
-    TestimonialsCarouselComponent, 
-    ServicesCarouselComponent, 
+    PortfolioCarouselComponent,
     DemoComponent,
-    AllSectionsComponent
   ],
   providers: [WINDOW_PROVIDERS]
 
